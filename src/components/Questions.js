63785516e08938a9b02b227e6e982e73edc49a4c
honogu <93793150+honogu@users.noreply.github.com>
2022-03-19 03:36:24 +0200

    update

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -7,19 +7,10 @@ const Questions = () => {
     const { language } = useContext(srcContext);
 
     return (
-        <div className='boxedd'>
-            <img src={praktikakohvik2019} alt="location"/>
+        <div className='container-questions'>
+            <img src={praktikakohvik2019} alt="praktikakohvik 2019"/>
 			<h3>{language.questions.questionTitle}</h3>
-			<h2>
-				<br/>{language.questions.questionContent.info}
-				<br/>{language.questions.questionContent.Question1}
-				<br/>{language.questions.questionContent.Question2}
-				<br/>{language.questions.questionContent.Question3}
-				<br/>{language.questions.questionContent.Question4}
-				<br/>{language.questions.questionContent.Question5}
-				<br/>{language.questions.questionContent.Question6}
-				<br/>{language.questions.questionContent.Question7}
-			</h2>
+			<h2>{language.questions.questionContent.replaceAll('\\n', '\n')}</h2>
         </div>
     )
 };
